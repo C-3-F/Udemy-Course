248da26c15c734285365dff2955be40d25635307
Cannon Farr <cantheman2468@gmail.com>
2019-07-09 15:28:16 -0600

    worked on redux-api

---
+++ redux-api/src/components/UserHeader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {fetchUser} from '../actions';
+
+class UserHeader extends Component {
+  render() {
+    return <div>User Header</div>;
+  }
+}
+
+export default connect(null, {fetchUser})(UserHeader);
